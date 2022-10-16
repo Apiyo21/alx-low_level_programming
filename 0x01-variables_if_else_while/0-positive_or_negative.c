eafe4601373105046d1ee273e7bb9602cfc1ac90
Memory Apiyo <apiyomemory28@gmail.com>
2022-10-16 19:49:27 +0300

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
-/* main - Positive anything is better than negative nothing */
-
+/**
+ *main - Positive anything is better than negative nothing
+ *Return: Always 0 (Success)
+ */
 
 int main(void)
 {
@@ -14,15 +16,15 @@ int main(void)
 
 	if (n > 0)
 	{
-		printf("%d is positive\n", n);
+		printf("%i is positive\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero\n", n);
+		printf("%i is zero\n", n);
 	}
 	else if (n < 0)
 	{
-		printf("%d is negative\n", n);
+		printf("%i is negative\n", n);
 	}
 
 	return (0);
