8fe086dda95f1bf2c52e82cfdcf764c740281d25
Memory Apiyo <apiyomemory28@gmail.com>
2022-10-16 19:45:25 +0300

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,63 +1,29 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <time.h>
 
-
-
-
-
-/**
- *
- *  *main - Positive anything is better than negative nothing
- *
- *   *Return: Always 0 (Success)
- *
- *    */
-
+/* main - Positive anything is better than negative nothing */
 
 
 int main(void)
-
 {
+	int n;
 
-		int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
 
-
-			srand(time(0));
-
-				n = rand() - RAND_MAX / 2;
-
-
-
-					if (n > 0)
-
-							{
-
-										printf("%d is positive\n", n);
-
-											}
-
-						else if (n == 0)
-
-								{
-
-											printf("%d is zero\n", n);
-
-												}
-
-							else if (n < 0)
-
-									{
-
-												printf("%d is negative\n", n);
-
-													}
-
-
-
-								return (0);
-
+	return (0);
 }
